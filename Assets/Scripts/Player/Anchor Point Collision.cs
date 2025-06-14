30d59c3f6f7e18a68375a4735e2ffee11544d827
wholesome-guy <vanshmunot264@gmail.com>
2025-06-14 19:22:14 +0530

    Reapply "Move ASTEROID to Behind of Player"
    This reverts commit 28d2fdfab3f92f7c44df09ee315184a6bb6bdd79.

--- Assets/Scripts/Player/Anchor Point Collision.cs
+++ Assets/Scripts/Player/Anchor Point Collision.cs
@@ -2,10 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
+using System;
 
 public class AnchorPointCollision : MonoBehaviour
 {
-    public static UnityEvent Tether_Asteroid;
+    public static Action Tether_Asteroid;
 
     [SerializeField] private string Asteroid_Tag = "Asteroid";
     private void OnTriggerEnter(Collider other)
