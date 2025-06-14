3fb768dd3eca4511bd9569cf9ad69566aa74e87a
wholesome-guy <vanshmunot264@gmail.com>
2025-06-14 17:11:15 +0530

    Move ASTEROID to Behind of Player
    FPS DROPPED TO 2

--- Assets/Scripts/Asteroid Script.cs
+++ Assets/Scripts/Asteroid Script.cs
@@ -10,12 +10,19 @@ public class AsteroidScript : MonoBehaviour
 
     [SerializeField] private float Asteroid_Speed = 100;
     [SerializeField] private float Asteroid_Rotation_Speed = 100;
+    [SerializeField] private float Pull_Force = 10;
 
-
+    private bool Is_Asteroid_Behind_SpaceShip = true;
     public bool Is_Asteroid_Tethered = false;
+    private void OnEnable()
+    {
+        AnchorPointCollision.Tether_Asteroid += Asteroid_Behind_Spaceship_Bool_Switch;
+    }
+    private void OnDisable()
+    {
+        AnchorPointCollision.Tether_Asteroid -= Asteroid_Behind_Spaceship_Bool_Switch;
+    }
 
-
-  
     void Start()
     {
         Asteroid_Mass = Asteroid_RigidBody.mass;
@@ -28,6 +35,10 @@ public class AsteroidScript : MonoBehaviour
             Asteroid_Movement();
         }
 
+        if(!Is_Asteroid_Behind_SpaceShip)
+        {
+            Asteroid_Positioner();
+        }
         
     }
 
@@ -39,7 +50,17 @@ public class AsteroidScript : MonoBehaviour
         Asteroid_RigidBody.AddTorque(Random_Vector * Asteroid_Rotation_Speed, ForceMode.Force);
     }
 
-   
-     
+   private void Asteroid_Positioner()
+   { 
+        Vector3 Asteroid_Point_Direction = PlayerSingleton.instance.Asteroid_Point.position - gameObject.transform.position;
+
+        Asteroid_RigidBody.AddForce(Asteroid_Point_Direction * Pull_Force,ForceMode.Force);
+   }
+
+    private void Asteroid_Behind_Spaceship_Bool_Switch()
+    {
+        Asteroid_RigidBody.velocity = Vector3.zero;
+        Is_Asteroid_Behind_SpaceShip = !Is_Asteroid_Behind_SpaceShip;
+    }
 
 }
